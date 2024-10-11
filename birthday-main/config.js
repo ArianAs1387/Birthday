93b414cf1a27311a0d81eda5109ae3c476d4380f
ArianAs1387 <asgariarian84@gmail.com>
2024-10-11 06:11:00 +0330

    Add files via upload

---
+++ birthday-main/config.js
@@ -0,0 +1,29 @@
+
+var config = {
+   
+    texts: [
+           
+        "happy birth day Amir", 
+        "Wishing you a day filled with love, cake, joy",
+        " and all the other wonderful things you deserve ",
+        "Have a great birthday",
+        " Enjoy your special day.)",
+    ],
+    // the upper text is in two languages vientamies and english 
+
+    imgs: {
+        "Châu": "./imgs/chau.jpg",
+  
+    },
+
+    desc: {
+        turn_on: "turn on the lights",
+        play: "play the music ",
+        bannar_coming: "surprise",
+        balloons_flying: "fly with ballons",
+        cake_fadein: "here's your cake",
+        light_candle: "Turn the candle on",
+        wish_message: "Happy Birthday",
+        story: "it's for you",
+    }
+};
